cee84a9042c31c93935cac79f79115f6f720aa58
yohan <rafael.hilario@oarenta.local>
2021-10-04 16:29:17 -0400

    Agregando archivos y Bootstrap

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,9 @@ router.get('/contact',(req, res) =>{
     res.render('contact', { title: 'Contact page'})
 })
 
+
+router.get('/about',(req, res) =>{
+    res.render('about', { title: 'About'})
+})
+
 module.exports = router
